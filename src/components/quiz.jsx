6d1f1c96743571acfd43783d2b31e37e5dbf46e1
Sanjana272621 <sanjana.sureshkumar05@gmail.com>
2025-06-08 22:12:34 +0530

    question bank

--- src/components/quiz.jsx
+++ src/components/quiz.jsx
@@ -3,21 +3,46 @@ import { useState } from 'react';
 function Quiz (){
     const questionBank = [
         {
-            question: "What is the capital of France?",
-            options: ["Paris", "Berlin", "Rome", "London"],
-            answer: "Paris"
+            question: "Which layer of the OSI model is responsible for end-to-end communication and error recovery?",
+            options: [
+                "Data Link Layer", 
+                "Network Layer", 
+                "Transport Layer", 
+                "Session Layer"],
+
+            answer: "Transport Layer"
         },
 
         {
-            question: "What is the capital of Italy?",
-            options: ["Paris", "Berlin", "Rome", "London"],
-            answer: "Rome"
+            question: "What is the primary purpose of the ARP (Address Resolution Protocol)?",
+            options: [
+                "To assign IP addresses dynamically" , 
+                "To resolve domain names to IP addresses" , 
+                "To map IP addresses to MAC addresses", 
+                "To route packets through different networks"],
+
+            answer: "To map IP addresses to MAC addresses",
+        },
+
+        {
+            question: " Which of the following IP addresses is a private IP address?",
+            options: [
+                "8.8.8.8" , 
+                "172.20.5.1" , 
+                "192.0.2.1", 
+                "169.255.255.255"],
+
+            answer: "172.20.5.1",
         }
     ];
 
+    const initialAnswers = [null, null, null];
+
+    const [] = useState();
+
+    //we have a state that keeps track of all the answers
     //Used to change the state for rendring new state in react 
     const [optionSelected, setOptionSelected] = useState("None");   // Initialized to None
-
 
     function handleSelectOption(option){
         setOptionSelected(option);
